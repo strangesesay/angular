d8a31610d60398e038061ecee59ad5207cf08bc8
strangesesay <126430875+strangesesay@users.noreply.github.com>
2025-08-25 15:44:10 +0000

    another commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import {Header} from "./component/header/header";
+import {ProductList} from './component/product-list/product-list';
 
 @Component({
   selector: 'app-root',
-  imports: [Header],
+  imports: [Header, ProductList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/component/product-list/product-list.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  imports: [],
+  templateUrl: './product-list.html',
+  styleUrl: './product-list.css'
+})
+export class ProductList {
+  products =
+    {
+      id: 1,
+      name: 'Wireless Mouse',
+      price: 25.99,
+      mimage: './assets/purple-computer-mouse.jpg'
+    }
+
+}
